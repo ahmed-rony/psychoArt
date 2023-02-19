87f72f58c8380ed70a7d0bd5ca6b794bf60eed7a
ahmed-rony <ahmedrony2101@gmail.com>
2023-02-20 01:59:51 +0600

    about done

--- src/Components/Component/About/About.jsx
+++ src/Components/Component/About/About.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './About.scss';
+import about01 from '../../Images/about.png';
 
 const About = () => {
     return (
         <div className='about'>
             <div className="header">
                 <h3>About Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit.</p>
             </div>
 
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6"></div>
                     <div className="col-md-6">
-                        <h4>Get Popular NFT</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. </p>
-                        
+                        <img src={about01} alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <div className="about-desc">
+                            <h4>Get Popular NFT</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. </p>
+                            <button className="brand-btn">Show more</button>
+                        </div>
                     </div>
                 </div>
             </div>
